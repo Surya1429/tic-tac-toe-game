c9fa33f1597036519a9cd72547de8eb887729bd5
vincent reddy <vincentreddy25@gmail.com>
2025-09-30 12:45:32 +0530

    modified the code in  score.java file by vincent

--- score.java
+++ score.java
@@ -3,5 +3,11 @@ class Score
 	public static void main(String args[])
 	{
 		System.out.println("tic tac game");
+
+		System.out.println("score board");
+		System.out.println("player 1 : 0");
+		System.out.println("player 2 : 0");
+		System.out.println("draw : 0");				
+		
 	}
 }
